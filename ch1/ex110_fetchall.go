e6b0ff7389d2faa6dbac91f2736721dfe56a9eb2
Akshay Gaikwad <akgaikwa@redhat.com>
2020-04-26 19:08:02 +0530

    Add fetchall Programs

---
+++ ch1/ex110_fetchall.go
@@ -0,0 +1,48 @@
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetch: %v\n", err)
+		os.Exit(1)
+	}
+	f, err := os.OpenFile("output.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	nbytes, err := io.Copy(f, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
